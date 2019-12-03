db267c0438b861240aff3db3656eab3da12e73bf
M. Edward (Ed) Borasky <znmeb@znmeb.net>
2019-12-03 05:15:21 +0000

    all functionality - stress test coming overnight

--- NDIA-census-tables/get-census-tables.R
+++ NDIA-census-tables/get-census-tables.R
@@ -6,6 +6,7 @@ if (!require(tidycensus)) install.packages("tidycensus")
 if (!require(tidyverse)) install.packages("tidyverse")
 if (!require(tigris)) install.packages("tigris")
 library(tidyverse)
+library(sf)
 
 # GIS setup
 tigris_cache <- "/Work/tigris_cache"
@@ -84,22 +85,28 @@ sf::st_write(
   driver = "GeoJSON",
   delete_dsn = TRUE
 )
+tract_cartographic_boundaries <- tract_cartographic_boundaries %>%
+  dplyr::select(geoid, geometry)
 sf::st_write(
   obj = county_cartographic_boundaries,
   dsn = paste0(output_directory, "/county_cartographic_boundaries.geojson"),
   driver = "GeoJSON",
   delete_dsn = TRUE
 )
-
+county_cartographic_boundaries <- county_cartographic_boundaries %>%
+  dplyr::select(geoid, geometry)
 # for (itable in internet_tables) {
 # for (itable in c("B28002", "B28010")) {
 for (itable in c("B28002")) {
 
-  geojson_dsn <- paste0(output_directory, "/", itable, ".geojson") # output GeoJSON file
+  tract_layer <- paste0(tolower(itable), "_tract")
+  county_layer <- paste0(tolower(itable), "_county")
+  tract_dsn <- paste0(output_directory, "/", tract_layer, ".geojson")
+  county_dsn <- paste0(output_directory, "/", county_layer, ".geojson")
 
   # we have to get the data one state at a time
   for (istate in unique(fips_codes$state)) {
-    work <- try(tidycensus::get_acs(
+    tract_work <- try(tidycensus::get_acs(
       geography = "tract",
       table = itable,
       year = 2017,
@@ -111,20 +118,62 @@ for (itable in c("B28002")) {
     ))
 
     # some states don't have data!
-    if (class(work) == "try-error") next
+    if (class(tract_work) == "try-error") next
 
     # clean names
-    work <- work %>% janitor::clean_names()
+    tract_work <- tract_work %>% janitor::clean_names()
+
+    # collect the state
     if (istate == "AL") {
-      full_table <- work
+      tract_long <- tract_work
     } else {
-      full_table <- rbind(full_table, work)
+      tract_long <- rbind(tract_long, tract_work)
     }
+
+    county_work <- try(tidycensus::get_acs(
+      geography = "county",
+      table = itable,
+      year = 2017,
+      output = "tidy",
+      state = istate,
+      geometry = FALSE,
+      moe_level = 90,
+      survey = "acs5"
+    ))
+    if (class(county_work) == "try-error") next
+    county_work <- county_work %>% janitor::clean_names()
+    if (istate == "AL") {
+      county_long <- county_work
+    } else {
+      county_long <- rbind(county_long, county_work)
+    }
+
   }
-  # sf::st_write(
-  #   obj = full_table,
-  #   dsn = geojson_dsn,
-  #   driver = "GeoJSON",
-  #   delete_dsn = TRUE
-  # )
+
+  tract_wide <- tract_long %>%
+    dplyr::inner_join(variable_decoder_ring, by = c("variable" = "name")) %>%
+    dplyr::select(-moe, -variable) %>%
+    tidyr::pivot_wider(names_from = column_header, values_from = estimate)
+  tract_wide <- tract_cartographic_boundaries %>% inner_join(tract_wide)
+  sf::st_write(
+    obj = tract_wide,
+    layer = tract_layer,
+    dsn = tract_dsn,
+    driver = "GeoJSON",
+    delete_dsn = TRUE
+  )
+
+  county_wide <- county_long %>%
+    dplyr::inner_join(variable_decoder_ring, by = c("variable" = "name")) %>%
+    dplyr::select(-moe, -variable) %>%
+    tidyr::pivot_wider(names_from = column_header, values_from = estimate)
+  county_wide <- county_cartographic_boundaries %>% inner_join(county_wide)
+  sf::st_write(
+    obj = county_wide,
+    layer = county_layer,
+    dsn = county_dsn,
+    driver = "GeoJSON",
+    delete_dsn = TRUE
+  )
+
 }
